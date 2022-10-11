a0e9d08147d44592394fe4c716c073dc7c0a5a1d
HaruoOgawa <haru.spring.yodai@gmail.com>
2022-10-11 18:59:55 +0900

    リリース準備周りを整備

--- setup.bat
+++ setup.bat
@@ -1,13 +1,5 @@
 set EnvPath=%~dp0
 
-set EnvPathDebug=%EnvPath%x64\Debug\bin
-set EnvPathRelease=%EnvPath%x64\Release\bin
-set EnvPathDemoRelease=%EnvPath%x64\DemoRelease\bin
+call %EnvPath%x64\setup_x64.bat
 
-setx /M Path "%Path%;%EnvPathDebug%;%EnvPathRelease%;%EnvPathDemoRelease%"
-
-@rem echo %EnvPathDebug%
-@rem echo %EnvPathRelease% 
-@rem echo %EnvPathDemoRelease%
- 
 @rem pause
