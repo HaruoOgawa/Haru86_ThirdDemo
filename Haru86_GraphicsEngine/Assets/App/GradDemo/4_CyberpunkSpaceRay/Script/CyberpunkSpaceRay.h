d96ae37f9d871cf6af7a264cf004ef43fb5abddd
HaruoOgawa <haru.spring.yodai@gmail.com>
2022-10-17 15:14:32 +0900

    CyberpunkSpaceRayのクラスを作成

---
+++ Haru86_GraphicsEngine/Assets/App/GradDemo/4_CyberpunkSpaceRay/Script/CyberpunkSpaceRay.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <memory>
+
+class MeshRendererComponent;
+namespace app
+{
+	class CyberpunkSpaceRay
+	{
+		std::shared_ptr<MeshRendererComponent> m_MeshRenderer;
+	public:
+		CyberpunkSpaceRay();
+		virtual ~CyberpunkSpaceRay() = default;
+
+		void Update();
+		void Draw(bool IsRaymarching);
+		void UpdateTimeLine(float time);
+	};
+}
+
